0a3979aa53118682812e1c17c5bc0c97dfc73c14
ramshad786 <ramshadramsha73@gmail.com>
2024-05-14 12:22:00 +0530

    test

---
+++ sum.py
@@ -0,0 +1,9 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=int(input("enter c value"))
+if (a>b)&(a>c):
+    print("%d is the biggest number"%(b))
+elif (b>a)&(b>c):
+    print("%d is the biggest number"%(b))
+else:
+    print("%d is the biggest number"%(b))
